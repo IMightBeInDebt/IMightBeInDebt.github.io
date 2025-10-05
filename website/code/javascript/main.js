8e38c8a06d8cbbd31dd6dcf90c945377b42aeb2c
imightbeindebt <h.a.judson@hotmail.com>
2025-10-05 18:26:58 -0500

    hold up

--- website/code/javascript/main.js
+++
@@ -1,9 +0,0 @@
-const feImage = document.querySelector('feImage');
-fetch("https://raw.githubusercontent.com/IMightBeInDebt/html-liquid-glass-thing/refs/heads/main/map.png")
-    .then((Response) => {
-        return Response.blob();
-    })
-    .then((blob) => {
-        const objURL = URL.createObjectURL(blob);
-        feImage.setAttribute("href", objURL);
-    });
